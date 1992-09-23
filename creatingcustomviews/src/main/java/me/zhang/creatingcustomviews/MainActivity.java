92d9d87e11922d19793a6e6e0ce6747c85238dbe
githuz <zxdeinstein@163.com>
1992-09-23 09:40:00 +0800

    Initial commit

---
+++ creatingcustomviews/src/main/java/me/zhang/creatingcustomviews/MainActivity.java
@@ -0,0 +1,35 @@
+package me.zhang.creatingcustomviews;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Zhang 3/1/15
+ */
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
