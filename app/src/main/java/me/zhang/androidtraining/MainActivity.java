798905beb7baae8aed315132a8714c47f2f09794
githuz <zxdeinstein@163.com>
2015-03-25 15:21:09 +0800

    A Simple Property Animation

--- app/src/main/java/me/zhang/androidtraining/MainActivity.java
+++ app/src/main/java/me/zhang/androidtraining/MainActivity.java
@@ -1,25 +1,21 @@
 package me.zhang.androidtraining;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class MainActivity extends ActionBarActivity {
+import me.zhang.property_animation.PropertyAnimatedActivity;
+
+public class MainActivity extends MenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+    }
+
+    @Override
+    void prepareMenu() {
+        addMenuItem("Property Animation", PropertyAnimatedActivity.class);
     }
 
     @Override
@@ -37,21 +33,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 }
