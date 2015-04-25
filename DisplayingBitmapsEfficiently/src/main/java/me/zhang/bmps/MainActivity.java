2f28d640a776d89c23d67108c0b9829896e64ea4
githuz <zxdeinstein@163.com>
2015-04-25 17:14:41 +0800

    Processing Bitmaps Off the UI Thread - Use an AsyncTask

--- DisplayingBitmapsEfficiently/src/main/java/me/zhang/bmps/MainActivity.java
+++ DisplayingBitmapsEfficiently/src/main/java/me/zhang/bmps/MainActivity.java
@@ -2,15 +2,19 @@ package me.zhang.bmps;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.lang.ref.WeakReference;
 
 import me.zhang.bmps.util.BitmapUtils;
 
@@ -18,11 +22,15 @@ import me.zhang.bmps.util.BitmapUtils;
 public class MainActivity extends ActionBarActivity {
 
     private static final int PICK_IMAGE = 0x10;
+    private ImageView mPickedImage;
+    private ProgressBar mLoadProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPickedImage = (ImageView) findViewById(R.id.picked_image);
+        mLoadProgress = (ProgressBar) findViewById(R.id.load_progress);
     }
 
     @Override
@@ -54,15 +62,58 @@ public class MainActivity extends ActionBarActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             if (data != null) {
-                try {
-                    InputStream in = getContentResolver().openInputStream(data.getData());
-                    Bitmap decoded = BitmapUtils.decodeSampledBitmapFromInputStream(in, 480, 800);
-                    ImageView pickedImage = (ImageView) findViewById(R.id.picked_image);
-                    pickedImage.setImageBitmap(decoded);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                Uri uri = data.getData();
+                loadBitmap(uri, mPickedImage);
+            }
+        }
+    }
+
+    private void loadBitmap(Uri uri, ImageView imageView) {
+        InputStream in = null;
+        try {
+            in = getContentResolver().openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        new BitmapWorkerTask(imageView).execute(in);
+    }
+
+    class BitmapWorkerTask extends AsyncTask<InputStream, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewWeakReference;
+
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage collected
+            imageViewWeakReference = new WeakReference<>(imageView);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (mLoadProgress != null) {
+                mLoadProgress.setVisibility(View.VISIBLE);
+            }
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(InputStream... params) {
+            return BitmapUtils.decodeSampledBitmapFromInputStream(params[0], 480, 800);
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (mLoadProgress != null) {
+                mLoadProgress.setVisibility(View.GONE);
+            }
+            if (bitmap != null) {
+                final ImageView imageView = imageViewWeakReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
                 }
             }
         }
     }
+
 }
