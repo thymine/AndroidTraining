e7fb46b7a6d067d705becd5966143d89880a3f7c
thymine <zxdeinstein@163.com>
2015-05-11 18:24:41 +0800

    Using ViewPager for Screen Slides - Customize the Animation with PageTransformer

--- AddingAnimations/src/main/java/com/soufun/animations/MainActivity.java
+++ AddingAnimations/src/main/java/com/soufun/animations/MainActivity.java
@@ -3,9 +3,10 @@ package com.soufun.animations;
 /**
  * Created by Zhang on 2015/5/11 下午 2:20 .
  */
-public class StartActivity extends MenuActivity {
+public class MainActivity extends MenuActivity {
     @Override
     protected void prepareMenu() {
         addMenuItem("CrossFading Two Views", FadeActivity.class);
+        addMenuItem("Using ViewPager for Screen Slides", ScreenSlidePageActivity.class);
     }
 }
