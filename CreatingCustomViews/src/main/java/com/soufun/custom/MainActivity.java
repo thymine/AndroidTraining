2469e41cacda01dd286b2731cd50d77845c07917
thymine <zxdeinstein@163.com>
2015-08-15 21:49:35 +0800

    Creating Custom Views

--- CreatingCustomViews/src/main/java/com/soufun/custom/MainActivity.java
+++ CreatingCustomViews/src/main/java/com/soufun/custom/MainActivity.java
@@ -1,33 +1,12 @@
 package com.soufun.custom;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-
-public class MainActivity extends ActionBarActivity {
-
+/**
+ * Created by Zhang on 8/15/2015 8:20 下午.
+ */
+public class MainActivity extends MenuActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void prepareMenu() {
+        addMenuItem("Pie Chart", PieActivity.class);
+        addMenuItem("Lovely View", LovelyActivity.class);
     }
 }
