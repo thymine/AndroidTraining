2898417998d11cd178b32b4e782deffe7cd2743a
thymine <zxdeinstein@163.com>
2015-10-20 18:36:37 +0800

    Adding none UI fragment to an activity

--- Lab/src/main/java/me/zhang/lab/fragment/ExampleTopFragment.java
+++ Lab/src/main/java/me/zhang/lab/fragment/ExampleTopFragment.java
@@ -1,6 +1,7 @@
 package me.zhang.lab.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -18,5 +19,16 @@ public class ExampleTopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_top_example, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* 跳转到非UI用途的Fragment示例页面 */
+                startActivity(new Intent(getActivity(), FragmentRetainInstanceActivity.class));
+            }
+        });
     }
 }
