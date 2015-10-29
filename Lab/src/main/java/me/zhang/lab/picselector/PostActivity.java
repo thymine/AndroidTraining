5dac6328f99d618a55b659ffc2e5377b52309d04
thymine <zxdeinstein@163.com>
2015-10-29 17:17:23 +0800

    Using Toolbar [menu->actionLayout, MenuItem->getActionView]

---
+++ Lab/src/main/java/me/zhang/lab/picselector/PostActivity.java
@@ -0,0 +1,73 @@
+package me.zhang.lab.picselector;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import me.zhang.lab.R;
+
+/**
+ * Created by Zhang on 2015/10/29 上午 9:54 .
+ */
+public class PostActivity extends BaseActivity {
+
+    private static final String TAG = "PostActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        // Toolbar is defined in the layout file
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("");
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Handle post button click callback
+    public void doPost(View view) {
+        Log.i(TAG, "Posting from onClick...");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Create your own menu
+        inflater.inflate(R.menu.menu_post, menu);
+
+        MenuItem postItem = menu.findItem(R.id.action_post);
+        View postView = postItem.getActionView();
+        Button postButton = (Button) postView.findViewById(R.id.btn_post);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Posting from ActionView...");
+            }
+        });
+
+        return true;
+    }
+}
