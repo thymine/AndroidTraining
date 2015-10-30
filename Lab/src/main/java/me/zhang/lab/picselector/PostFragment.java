42d54791dea2bc31e57d272f28419ad05a391bd9
thymine <zxdeinstein@163.com>
2015-10-30 18:23:27 +0800

    Create GridView Adapter

--- Lab/src/main/java/me/zhang/lab/picselector/PostFragment.java
+++ Lab/src/main/java/me/zhang/lab/picselector/PostFragment.java
@@ -1,15 +1,58 @@
 package me.zhang.lab.picselector;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.ClipData;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import me.zhang.lab.R;
 
 /**
  * Created by Zhang on 2015/10/29 下午 5:19 .
  */
-public class PostFragment extends BaseFragment {
+public class PostFragment extends BaseFragment implements SelectedPicsAdapter.OnAddButtonClickListener {
+
+    private static final String TAG = "PostFragment";
+    private static final int RESULT_LOAD_IMAGE = 7;
+
+    @Bind(R.id.et_content)
+    EditText content;
+
+    @Bind(R.id.vv_video)
+    VideoView video;
+
+    @Bind(R.id.gv_pics_selected)
+    GridView selPictures;
+
+    @Bind(R.id.iv_location)
+    ImageView locationIcon;
+
+    @Bind(R.id.tv_selected_location)
+    TextView selLocation;
+
+    private List<ImageItem> imageItemList = new ArrayList<>();
+    private SelectedPicsAdapter selectedPicsAdapter;
+
+    public static Fragment newInstance(Bundle args) {
+        Fragment fragment = new PostFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     protected int getLayoutResourceId() {
@@ -18,12 +61,42 @@ public class PostFragment extends BaseFragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        // The view was created, do what you want
+        ButterKnife.bind(this, view);
+
+        selectedPicsAdapter = new SelectedPicsAdapter(getActivity(), imageItemList, this);
+        selPictures.setAdapter(selectedPicsAdapter);
+        selPictures.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "onItemClick, position:" + position);
+            }
+        });
     }
 
-    public static Fragment newInstance(Bundle args) {
-        Fragment fragment = new PostFragment();
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == RESULT_LOAD_IMAGE) {
+                /* Add selected pictures to list */
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        Uri uri = clipData.getItemAt(i).getUri();
+                        ImageItem item = new ImageItem();
+                        item.path = uri;
+                        imageItemList.add(item);
+                    }
+                    selectedPicsAdapter.notifyDataSetChanged();
+                } else {
+                    Log.i(TAG, "clipData is null");
+                }
+            }
+        }
     }
+
+    @Override
+    public void onAddButtonClick() {
+        // Jump to gallery and select multiple images
+    }
+
 }
