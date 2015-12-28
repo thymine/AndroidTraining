d3ce363a104b2966c85776cdfb84dfec85520004
thymine <zxdeinstein@163.com>
2015-12-28 09:51:09 +0800

    自定义控件：ColorFilter

--- Lab/src/main/java/me/zhang/lab/view/CustomView.java
+++ Lab/src/main/java/me/zhang/lab/view/CustomView.java
@@ -1,19 +1,24 @@
 package me.zhang.lab.view;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import me.zhang.lab.R;
 import me.zhang.lab.utils.HardwareUtils;
 
 /**
  * Created by Zhang on 2015/12/24 上午 10:51 .
  */
-public class CustomView extends View implements Runnable {
+public class CustomView extends View {
     private static final String TAG = CustomView.class.getSimpleName();
 
     private Paint paint;
@@ -21,9 +26,7 @@ public class CustomView extends View implements Runnable {
     private int cx, cy; // 圆环圆心坐标
 
     private int radius; // 圆环半径
-    private int direction = 1;
-    private static final int MAX_RADIUS = 400;
-    private static final int RATE = 3;
+    private Bitmap bitmap;
 
     public CustomView(Context context) {
         super(context);
@@ -36,6 +39,14 @@ public class CustomView extends View implements Runnable {
     }
 
     private void init() {
+        int[] dimens = new int[2];
+        HardwareUtils.getScreenDimens(context, dimens);
+        cx = dimens[0] / 2;
+        cy = dimens[1] / 2;
+        Log.i(TAG, "cx: " + cx + ", cy: " + cy);
+
+        bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.pizza);
+
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
         /*
@@ -46,8 +57,10 @@ public class CustomView extends View implements Runnable {
          * 2.Paint.Style.FILL_AND_STROKE：描边并填充
          * 3.Paint.Style.FILL：填充
          */
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.LTGRAY);
+        paint.setStyle(Paint.Style.FILL);
+
+        // 设置画笔颜色为自定义颜色
+        paint.setColor(Color.argb(255, 255, 128, 103));
 
         /*
          * 设置描边的粗细，单位：像素px
@@ -55,18 +68,30 @@ public class CustomView extends View implements Runnable {
          */
         paint.setStrokeWidth(10);
 
-        int[] dimens = new int[2];
-        HardwareUtils.getScreenDimens(context, dimens);
-        cx = dimens[0] / 2;
-        cy = dimens[1] / 2;
-        Log.i(TAG, "cx: " + cx + ", cy: " + cy);
+        // 生成色彩矩阵
+        ColorMatrix colorMatrix = new ColorMatrix(new float[]{
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0,
+        });
+//        colorMatrix = new ColorMatrix(new float[]{
+//                0.5f, 0, 0, 0, 0,
+//                0, 0.5f, 0, 0, 0,
+//                0, 0, 0.5f, 0, 0,
+//                0, 0, 0, 1, 0,
+//        });
+        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(cx, cy, radius, paint);
+        canvas.drawCircle(cx, cy, 400, paint);
         canvas.drawLine(0, cy, 2 * cx, cy, paint);
         canvas.drawLine(cx, 0, cx, 2 * cy, paint);
+
+        canvas.drawBitmap(bitmap, cx - bitmap.getWidth() / 2, cy - bitmap.getHeight() / 2, paint);
     }
 
     public synchronized void setRadius(int radius) {
@@ -74,31 +99,4 @@ public class CustomView extends View implements Runnable {
         invalidate();
     }
 
-    @Override
-    public void run() {
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            if (direction > 0) {
-                if (radius <= MAX_RADIUS) {
-                    radius += RATE;
-                } else {
-                    direction = -1;
-                }
-            } else {
-                if (radius >= 0) {
-                    radius -= RATE;
-                } else {
-                    direction = 1;
-                }
-            }
-
-            postInvalidate();
-
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
