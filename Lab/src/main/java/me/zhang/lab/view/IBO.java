4c58a65c3a08f05d6c6e7dde017c2c2ea8124343
thymine <zxdeinstein@163.com>
2016-01-08 17:59:23 +0800

    自定义View控件：1/6

---
+++ Lab/src/main/java/me/zhang/lab/view/IBO.java
@@ -0,0 +1,12 @@
+package me.zhang.lab.view;
+
+/**
+ * 业务逻辑层接口
+ * 
+ * @author Aige
+ * @since 2014/11/19
+ * 
+ */
+public interface IBO {
+
+}
---
+++ Lab/src/main/java/me/zhang/lab/view/PorterDuffBO.java
@@ -0,0 +1,57 @@
+package me.zhang.lab.view;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.util.Log;
+
+/**
+ * PorterDuffView的业务逻辑对象
+ * 不用管我是如何实现的你只管调用即可
+ *
+ * @author Aige
+ * @since 2014/11/19
+ */
+public class PorterDuffBO implements IBO {
+    private static final String TAG = "PorterDuffBO";
+    private int size;
+    private Point mPoint;
+
+    public PorterDuffBO() {
+        mPoint = new Point();
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+        mPoint.set(size, size);
+    }
+
+    public Bitmap initSrcBitmap() {
+        int[] pixels = new int[mPoint.x * mPoint.y];
+        int dst = 0;
+        for (int row = 0; row < mPoint.y; ++row) {
+            for (int col = 0; col < mPoint.x; ++col) {
+                int color = color((float) (mPoint.y - row) / mPoint.y, (float) (mPoint.x - col) / mPoint.x, (float) (mPoint.x - col) / mPoint.x, (float) col / mPoint.x);
+                Log.i(TAG, "[" + row + ", " + col + "] src color: " + color);
+                pixels[dst++] = color;
+            }
+        }
+        return Bitmap.createBitmap(pixels, size, size, Bitmap.Config.ARGB_8888);
+    }
+
+    public Bitmap initDisBitmap() {
+        int[] pixels = new int[mPoint.x * mPoint.y];
+        int dst = 0;
+        for (int row = 0; row < mPoint.y; ++row) {
+            for (int col = 0; col < mPoint.x; ++col) {
+                int color = color((float) (mPoint.x - col) / mPoint.x, (float) (mPoint.y - row) / mPoint.x, (float) row / mPoint.y, (float) row / mPoint.y);
+                Log.i(TAG, "[" + row + ", " + col + "] dst color: " + color);
+                pixels[dst++] = color;
+            }
+        }
+        return Bitmap.createBitmap(pixels, size, size, Bitmap.Config.ARGB_8888);
+    }
+
+    private int color(float Alpha, float R, float G, float B) {
+        return (int) (Alpha * 255) << 24 | (int) (R * Alpha * 255) << 16 | (int) (G * Alpha * 255) << 8 | (int) (B * Alpha * 255);
+    }
+}
