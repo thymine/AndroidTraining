b06176554efd4133aabd2d470faa3c3f015fe843
thymine <zxdeinstein@163.com>
2016-01-18 13:47:57 +0800

    BitmapShader

--- Lab/src/main/java/me/zhang/lab/view/ui/CustomViewActivity.java
+++ Lab/src/main/java/me/zhang/lab/view/ui/CustomViewActivity.java
@@ -41,6 +41,7 @@ public class CustomViewActivity extends AppCompatActivity {
         fragments.add(ECGViewFragment.newInstance());
         fragments.add(ShadowViewFragment.newInstance());
         fragments.add(ShaderViewFragment.newInstance());
+        fragments.add(BrickViewFragment.newInstance());
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
