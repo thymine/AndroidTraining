93dac89d3967adba3435283e961bed7778410cd6
thymine <zxdeinstein@163.com>
2016-01-18 14:14:39 +0800

    Reflection effect

---
+++ Lab/src/main/java/me/zhang/lab/view/ReflectView.java
@@ -0,0 +1,88 @@
+package me.zhang.lab.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.view.View;
+
+import me.zhang.lab.R;
+import me.zhang.lab.utils.MeasureUtil;
+
+/**
+ * 反射效果
+ *
+ * @author Aige
+ * @since 2014/11/25
+ */
+public class ReflectView extends View {
+    private Bitmap mSrcBitmap, mRefBitmap;// 位图
+    private Paint mPaint;// 画笔
+    private PorterDuffXfermode mXfermode;// 混合模式
+
+    private int x, y;// 位图起点坐标
+
+    public ReflectView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // 初始化资源
+        initRes(context);
+    }
+
+    /*
+     * 初始化资源
+     */
+    private void initRes(Context context) {
+        // 获取源图
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        mSrcBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.girl, options);
+
+        // 实例化一个矩阵对象
+        Matrix matrix = new Matrix();
+        matrix.setScale(1F, -1F);
+
+        // 生成倒影图
+        mRefBitmap = Bitmap.createBitmap(mSrcBitmap, 0, 0, mSrcBitmap.getWidth(), mSrcBitmap.getHeight(), matrix, true);
+
+        int screenW = MeasureUtil.getScreenSize((Activity) context)[0];
+        int screenH = MeasureUtil.getScreenSize((Activity) context)[1];
+
+        x = screenW / 8;
+        y = screenH / 8;
+
+        // ………………………………
+        mPaint = new Paint();
+        mPaint.setShader(new LinearGradient(x, y + mSrcBitmap.getHeight(), x, y + mSrcBitmap.getHeight() + mSrcBitmap.getHeight() / 4, 0xAA000000, Color.TRANSPARENT, Shader.TileMode.CLAMP));
+
+        // ………………………………
+        mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLACK);
+        canvas.drawBitmap(mSrcBitmap, x, y, null);
+
+        int sc = canvas.saveLayer(x, y + mSrcBitmap.getHeight(), x + mRefBitmap.getWidth(), y + mSrcBitmap.getHeight() * 2, null, Canvas.ALL_SAVE_FLAG);
+
+        canvas.drawBitmap(mRefBitmap, x, y + mSrcBitmap.getHeight(), null);
+
+        mPaint.setXfermode(mXfermode);
+
+        canvas.drawRect(x, y + mSrcBitmap.getHeight(), x + mRefBitmap.getWidth(), y + mSrcBitmap.getHeight() * 2, mPaint);
+
+        mPaint.setXfermode(null);
+
+        canvas.restoreToCount(sc);
+    }
+}
