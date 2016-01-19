e840143883298c2c8f31f96c1c409d07343687b2
thymine <zxdeinstein@163.com>
2016-01-19 16:25:16 +0800

    DreamEffectView

---
+++ Lab/src/main/java/me/zhang/lab/view/ui/DreamEffectViewFragment.java
@@ -0,0 +1,27 @@
+package me.zhang.lab.view.ui;
+
+import me.zhang.lab.R;
+
+
+/**
+ * Created by Zhang on 2015/12/24 上午 10:33 .
+ */
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DreamEffectViewFragment extends BaseFragment {
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static DreamEffectViewFragment newInstance() {
+        return new DreamEffectViewFragment();
+    }
+
+    @Override
+    protected int supplyLayoutResource() {
+        return R.layout.fragment_dream_effect;
+    }
+}
