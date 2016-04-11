d8da49ea849c1d8a7f64355fdc36664a2b77f64f
thymine <zxdeinstein@163.com>
2016-04-11 18:15:54 +0800

    overwrite onTouchEvent()

--- Workbench/src/main/java/me/zhang/workbench/gestures/GesturesActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/gestures/GesturesActivity.java
@@ -1,19 +1,14 @@
 package me.zhang.workbench.gestures;
 
 import android.os.Bundle;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import me.zhang.workbench.R;
 
 public class GesturesActivity extends AppCompatActivity {
 
-    private static final String DEBUG_TAG = GesturesActivity.class.getSimpleName();
+    public static final String DEBUG_TAG = GesturesActivity.class.getSimpleName();
     private boolean touch = false;
 
     @Override
@@ -21,28 +16,46 @@ public class GesturesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestures);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return touch;
-            }
-        });
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(GesturesActivity.this, "onClick()", Toast.LENGTH_SHORT).show();
-                Log.d(DEBUG_TAG, "onClick()");
-            }
-        });
-        button.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(GesturesActivity.this, "onLongClick()", Toast.LENGTH_SHORT).show();
-                Log.d(DEBUG_TAG, "onLongClick()");
-                return false;
-            }
-        });
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                int action = MotionEventCompat.getActionMasked(event);
+//                switch (action) {
+//                    case (MotionEvent.ACTION_DOWN):
+//                        Log.d(DEBUG_TAG, "onTouch() DOWN");
+//                        return false;
+//                    case (MotionEvent.ACTION_MOVE):
+//                        Log.d(DEBUG_TAG, "onTouch() MOVE");
+//                        break;
+//                    case (MotionEvent.ACTION_UP):
+//                        Log.d(DEBUG_TAG, "onTouch() UP");
+//                        break;
+//                    case (MotionEvent.ACTION_CANCEL):
+//                        Log.d(DEBUG_TAG, "onTouch() CANCEL");
+//                        break;
+//                    case (MotionEvent.ACTION_OUTSIDE):
+//                        Log.d(DEBUG_TAG, "onTouch() Movement occurred outside bounds of current screen element");
+//                        break;
+//                }
+//                return touch;
+//            }
+//        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(GesturesActivity.this, "onClick()", Toast.LENGTH_SHORT).show();
+//                Log.d(DEBUG_TAG, "onClick()");
+//            }
+//        });
+//        button.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                Toast.makeText(GesturesActivity.this, "onLongClick()", Toast.LENGTH_SHORT).show();
+//                Log.d(DEBUG_TAG, "onLongClick()");
+//                return false;
+//            }
+//        });
     }
 
     public void onTouchToggle(View view) {
@@ -56,48 +69,47 @@ public class GesturesActivity extends AppCompatActivity {
 //
 //        switch (action) {
 //            case (MotionEvent.ACTION_DOWN):
-//                Log.d(DEBUG_TAG, "DOWN");
+//                Log.d(DEBUG_TAG, "dispatchTouchEvent() DOWN");
 //                return true;
 //            case (MotionEvent.ACTION_MOVE):
-//                Log.d(DEBUG_TAG, "MOVE");
+//                Log.d(DEBUG_TAG, "dispatchTouchEvent() MOVE");
 //                return true;
 //            case (MotionEvent.ACTION_UP):
-//                Log.d(DEBUG_TAG, "UP");
+//                Log.d(DEBUG_TAG, "dispatchTouchEvent() UP");
 //                return true;
 //            case (MotionEvent.ACTION_CANCEL):
-//                Log.d(DEBUG_TAG, "CANCEL");
+//                Log.d(DEBUG_TAG, "dispatchTouchEvent() CANCEL");
 //                return true;
 //            case (MotionEvent.ACTION_OUTSIDE):
-//                Log.d(DEBUG_TAG, "Movement occurred outside bounds of current screen element");
+//                Log.d(DEBUG_TAG, "dispatchTouchEvent() Movement occurred outside bounds of current screen element");
 //                return true;
 //            default:
 //                return super.onTouchEvent(event);
 //        }
 //    }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.d(DEBUG_TAG, "onTouchEvent(...)");
-        int action = MotionEventCompat.getActionMasked(event);
-
-        switch (action) {
-            case (MotionEvent.ACTION_DOWN):
-                Log.d(DEBUG_TAG, "DOWN");
-                return true;
-            case (MotionEvent.ACTION_MOVE):
-                Log.d(DEBUG_TAG, "MOVE");
-                return true;
-            case (MotionEvent.ACTION_UP):
-                Log.d(DEBUG_TAG, "UP");
-                return true;
-            case (MotionEvent.ACTION_CANCEL):
-                Log.d(DEBUG_TAG, "CANCEL");
-                return true;
-            case (MotionEvent.ACTION_OUTSIDE):
-                Log.d(DEBUG_TAG, "Movement occurred outside bounds of current screen element");
-                return true;
-            default:
-                return super.onTouchEvent(event);
-        }
-    }
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        int action = MotionEventCompat.getActionMasked(event);
+//
+//        switch (action) {
+//            case (MotionEvent.ACTION_DOWN):
+//                Log.d(DEBUG_TAG, "onTouchEvent() DOWN");
+//                return true;
+//            case (MotionEvent.ACTION_MOVE):
+//                Log.d(DEBUG_TAG, "onTouchEvent() MOVE");
+//                return true;
+//            case (MotionEvent.ACTION_UP):
+//                Log.d(DEBUG_TAG, "onTouchEvent() UP");
+//                return true;
+//            case (MotionEvent.ACTION_CANCEL):
+//                Log.d(DEBUG_TAG, "onTouchEvent() CANCEL");
+//                return true;
+//            case (MotionEvent.ACTION_OUTSIDE):
+//                Log.d(DEBUG_TAG, "onTouchEvent() Movement occurred outside bounds of current screen element");
+//                return true;
+//            default:
+//                return super.onTouchEvent(event);
+//        }
+//    }
 }
---
+++ Workbench/src/main/java/me/zhang/workbench/gestures/MyButton.java
@@ -0,0 +1,50 @@
+package me.zhang.workbench.gestures;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+import static me.zhang.workbench.gestures.GesturesActivity.DEBUG_TAG;
+
+/**
+ * Created by Zhang on 2016/4/11 下午 6:00 .
+ */
+public class MyButton extends Button {
+    public MyButton(Context context) {
+        super(context);
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = MotionEventCompat.getActionMasked(event);
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                Log.d(DEBUG_TAG, "onTouchEvent() DOWN");
+                return false;
+            case (MotionEvent.ACTION_MOVE):
+                Log.d(DEBUG_TAG, "onTouchEvent() MOVE");
+                break;
+            case (MotionEvent.ACTION_UP):
+                Log.d(DEBUG_TAG, "onTouchEvent() UP");
+                break;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(DEBUG_TAG, "onTouchEvent() CANCEL");
+                break;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(DEBUG_TAG, "onTouchEvent() Movement occurred outside bounds of current screen element");
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
