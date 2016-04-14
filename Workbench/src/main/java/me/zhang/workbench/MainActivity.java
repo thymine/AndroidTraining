b61c7b0240fa1267f6578716e167ec2b2e85feef
thymine <zxdeinstein@163.com>
2016-04-14 17:05:21 +0800

    Adding items dynamically to ScrollView

--- Workbench/src/main/java/me/zhang/workbench/MainActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/MainActivity.java
@@ -5,6 +5,7 @@ import me.zhang.workbench.animation.CardFlipActivity;
 import me.zhang.workbench.animation.LogoWhiteAnimActivity;
 import me.zhang.workbench.base.MenuActivity;
 import me.zhang.workbench.gestures.GesturesActivity;
+import me.zhang.workbench.layout.LayoutActivity;
 import me.zhang.workbench.view.VisibilityActivity;
 import me.zhang.workbench.webview.WebViewActivity;
 
@@ -12,6 +13,7 @@ public class MainActivity extends MenuActivity {
 
     @Override
     protected void prepareMenu() {
+        addMenuItem("Layout", LayoutActivity.class);
         addMenuItem("WebView", WebViewActivity.class);
         addMenuItem("GesturesActivity", GesturesActivity.class);
         addMenuItem("Visibility", VisibilityActivity.class);
