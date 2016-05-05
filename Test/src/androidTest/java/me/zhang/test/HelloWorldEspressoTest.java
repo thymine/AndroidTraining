5ed400844db1a739acd0f579ec0cffc3fcd9735f
thymine <zxdeinstein@163.com>
2016-05-05 17:28:04 +0800

    Testing Button Clickable

--- Test/src/androidTest/java/me/zhang/test/HelloWorldEspressoTest.java
+++ Test/src/androidTest/java/me/zhang/test/HelloWorldEspressoTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -26,6 +27,11 @@ public class HelloWorldEspressoTest {
     @Test
     public void listGoesOverTheFold() {
         onView(withText("Hello World!")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void buttonClickable() {
+        onView(isClickable()).check(matches(isClickable()));
     }
 
 }
