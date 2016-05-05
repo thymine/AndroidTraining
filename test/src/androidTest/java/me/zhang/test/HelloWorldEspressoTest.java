023173bb211687d1772e4ded6d9b7f376bc5223d
thymine <zxdeinstein@163.com>
2016-05-05 16:56:01 +0800

    Adding the first test

---
+++ test/src/androidTest/java/me/zhang/test/HelloWorldEspressoTest.java
@@ -0,0 +1,31 @@
+package me.zhang.test;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Zhang on 2016/5/5 下午 4:43 .
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class HelloWorldEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("Hello world")).check(matches(isDisplayed()));
+    }
+
+}
