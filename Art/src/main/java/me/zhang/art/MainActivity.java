41fd9ff3549de6e936c1316a405c71c2313033ef
clsera <zxdeinstein@163.com>
2016-06-05 13:30:56 +0800

    Android Art

---
+++ Art/src/main/java/me/zhang/art/MainActivity.java
@@ -0,0 +1,12 @@
+package me.zhang.art;
+
+import me.zhang.art.base.MenuActivity;
+
+public class MainActivity extends MenuActivity {
+
+    @Override
+    protected void prepareMenu() {
+
+    }
+
+}
