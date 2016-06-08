5d954831bd076fa197d9e51ca4a000ce013ba8a4
clsera <zxdeinstein@163.com>
2016-06-08 21:57:39 +0800

    resolveActivity

--- Art/src/main/java/me/zhang/art/activity/intentfilter/IntentFilterActivity.java
+++ Art/src/main/java/me/zhang/art/activity/intentfilter/IntentFilterActivity.java
@@ -1,13 +1,16 @@
 package me.zhang.art.activity.intentfilter;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
@@ -52,8 +55,35 @@ public class IntentFilterActivity extends AppCompatActivity {
             }
         });
 
+        Button dataButton = new Button(this);
+        dataButton.setText("data filter");
+        dataButton.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ACTION_OPEN).setDataAndType(Uri.parse("file://abc"), "image/png"));
+            }
+        });
+
+        Button noButton = new Button(this);
+        noButton.setText("no activity");
+        noButton.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ComponentName componentName = new Intent(ACTION_OPEN).setDataAndType(Uri.parse("file://abc"), "image/png").resolveActivity(getPackageManager());
+                if (componentName == null) {
+                    Toast.makeText(IntentFilterActivity.this, "No Activity found", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(IntentFilterActivity.this, componentName.getShortClassName(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         container.addView(actionButton);
         container.addView(categoryButton);
+        container.addView(dataButton);
+        container.addView(noButton);
 
         setContentView(container);
     }
