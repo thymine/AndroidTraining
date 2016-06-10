318ca196bdf62db8be1747eb4416655614456089
clsera <zxdeinstein@163.com>
2016-06-10 14:00:24 +0800

    http://stackoverflow.com/a/1944564

---
+++ Art/src/main/java/me/zhang/art/ipc/IpcActivity.java
@@ -0,0 +1,15 @@
+package me.zhang.art.ipc;
+
+import me.zhang.art.base.MenuActivity;
+
+/**
+ * Created by Li on 6/9/2016 12:22 PM.
+ */
+public class IPCActivity extends MenuActivity {
+
+    @Override
+    protected void prepareMenu() {
+        addMenuItem("MultiProcess", DefaultProcessActivity.class);
+    }
+
+}
--- Art/src/main/java/me/zhang/art/ipc/SecondProcessActivity.java
+++ Art/src/main/java/me/zhang/art/ipc/SecondProcessActivity.java
@@ -8,9 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Locale;
+
+import static android.view.Gravity.CENTER;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import static android.widget.LinearLayout.VERTICAL;
+import static me.zhang.art.ipc.UserManager.sUserId;
 
 /**
  * Created by Li on 6/9/2016 10:54 AM.
@@ -24,6 +31,9 @@ public class SecondProcessActivity extends AppCompatActivity {
 
         setTitle("SecondProcessActivity");
 
+        LinearLayout container = new LinearLayout(this);
+        container.setOrientation(VERTICAL);
+
         Button openThirdProcessButton = new Button(this);
         openThirdProcessButton.setText("Open third process");
         openThirdProcessButton.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
@@ -34,7 +44,29 @@ public class SecondProcessActivity extends AppCompatActivity {
             }
         });
 
-        setContentView(openThirdProcessButton);
+        final TextView userIdText = new TextView(this);
+        userIdText.setText(String.format(Locale.getDefault(), "User Id is %d", sUserId));
+        userIdText.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        userIdText.setGravity(CENTER);
+        userIdText.setTextSize(32);
+
+        Button setUserIdButton = new Button(this);
+        setUserIdButton.setText("Set User Id to 2");
+        setUserIdButton.setLayoutParams(new LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT));
+        setUserIdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserManager.sUserId = 2;
+                userIdText.setText(String.format(Locale.getDefault(), "User Id is %d", sUserId));
+                Toast.makeText(SecondProcessActivity.this, "User Id is 2 now", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        container.addView(openThirdProcessButton);
+        container.addView(setUserIdButton);
+        container.addView(userIdText);
+
+        setContentView(container);
     }
 
 }
