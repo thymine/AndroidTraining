f32c47fafa0a84e4c84f25b859a8177567fddd2b
clsera <zxdeinstein@163.com>
2016-06-25 10:12:51 +0800

    Scroll content by Animator

--- Workbench/src/main/java/me/zhang/workbench/view/SmoothScrollActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/view/SmoothScrollActivity.java
@@ -1,30 +1,41 @@
 package me.zhang.workbench.view;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import me.zhang.workbench.R;
+
+import static android.animation.ValueAnimator.INFINITE;
+import static android.animation.ValueAnimator.REVERSE;
 
 /**
  * Created by Li on 6/24/2016 9:53 PM.
  */
 public class SmoothScrollActivity extends AppCompatActivity {
 
-    private SmoothView smoothView;
+    private static final String TAG = SmoothScrollActivity.class.getSimpleName();
+    public static final int DEST_Y = 400;
+    private SmoothView scrollerSmoothView;
+    private SmoothView animationSmoothView;
+
+    private int startY = 0;
+    private int deltaY = DEST_Y;
 
     private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
             switch (msg.what) {
                 case 1:
-                    smoothView.smoothScrollTo(0, 0);
+                    scrollerSmoothView.smoothScrollTo(0, 0);
                     break;
                 case -1:
-                    smoothView.smoothScrollTo(0, smoothView.getHeight());
+                    scrollerSmoothView.smoothScrollTo(0, DEST_Y);
                     break;
             }
             return false;
@@ -36,8 +47,9 @@ public class SmoothScrollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smooth_scroll);
 
-        smoothView = (SmoothView) findViewById(R.id.smoothView);
-        assert smoothView != null;
+        //region Use Scroller
+        scrollerSmoothView = (SmoothView) findViewById(R.id.scrollerSmoothView);
+        assert scrollerSmoothView != null;
 
         new Thread(new Runnable() {
 
@@ -59,6 +71,24 @@ public class SmoothScrollActivity extends AppCompatActivity {
             }
 
         }).start();
+        //endregion
+
+        //region Use Animator
+        animationSmoothView = (SmoothView) findViewById(R.id.animationSmoothView);
+        final ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 1).setDuration(1000);
+        valueAnimator.setRepeatCount(INFINITE);
+        valueAnimator.setRepeatMode(REVERSE);
+
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float fraction = valueAnimator.getAnimatedFraction();
+                Log.i(TAG, "onAnimationUpdate: fraction = " + fraction);
+                animationSmoothView.scrollTo(0, (int) (deltaY * fraction) + startY);
+            }
+        });
+        valueAnimator.start();
+        //endregion
     }
 
 }
