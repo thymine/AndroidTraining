1c5611b7793c85d45a7d896deeeb22671c07f65b
clsera <zxdeinstein@163.com>
2016-07-05 21:12:18 +0800

    Support wrap_content and padding properties

---
+++ Workbench/src/main/java/me/zhang/workbench/ui/CircleViewActivity.java
@@ -0,0 +1,19 @@
+package me.zhang.workbench.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import me.zhang.workbench.R;
+
+/**
+ * Created by Li on 7/5/2016 8:54 PM.
+ */
+public class CircleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_view);
+    }
+}
