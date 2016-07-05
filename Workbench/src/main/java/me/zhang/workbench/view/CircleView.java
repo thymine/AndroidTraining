88bb8051afcda0f6133e55e29662b90b73c63453
clsera <zxdeinstein@163.com>
2016-07-05 21:12:18 +0800

    Support wrap_content and padding properties

---
+++ Workbench/src/main/java/me/zhang/workbench/view/CircleView.java
@@ -0,0 +1,64 @@
+package me.zhang.workbench.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import static android.graphics.Color.RED;
+import static android.graphics.Paint.ANTI_ALIAS_FLAG;
+import static android.view.View.MeasureSpec.AT_MOST;
+import static android.view.View.MeasureSpec.getMode;
+import static android.view.View.MeasureSpec.getSize;
+import static java.lang.Math.min;
+
+/**
+ * Created by Li on 7/5/2016 8:50 PM.
+ */
+public class CircleView extends View {
+
+    public static final int DEFAULT_DIMEN = 256;
+
+    private int mColor = RED;
+    private Paint mPaint = new Paint(ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint.setColor(mColor);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int widthSpecMode = getMode(widthMeasureSpec);
+        int widthSpecSize = getSize(widthMeasureSpec);
+        int heightSpecMode = getMode(heightMeasureSpec);
+        int heightSpecSize = getSize(heightMeasureSpec);
+
+        // set default dimens for "wrap_content"
+        if (widthSpecMode == AT_MOST && heightSpecMode == AT_MOST) {
+            setMeasuredDimension(DEFAULT_DIMEN, DEFAULT_DIMEN);
+        } else if (widthSpecMode == AT_MOST) {
+            setMeasuredDimension(DEFAULT_DIMEN, heightSpecSize);
+        } else if (heightSpecMode == AT_MOST) {
+            setMeasuredDimension(widthSpecSize, DEFAULT_DIMEN);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // do not forget paddings
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int width = getWidth() - paddingLeft - paddingRight; // remove horizontal paddings
+        int height = getHeight() - paddingTop - paddingBottom; // remove vertical paddings
+
+        int radius = min(width, height) / 2;
+        canvas.drawCircle(width / 2 + paddingLeft, height / 2 + paddingTop, radius, mPaint);
+    }
+}
