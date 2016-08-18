cbf53b56a583c5874d5e8a796c7c6ed23447df58
clsera <zxdeinstein@163.com>
2016-08-18 21:59:07 +0800

    Use RemoteViews Build App Widgets

---
+++ Workbench/src/main/java/me/zhang/workbench/remoteViews/MyAppWidgetProvider.java
@@ -0,0 +1,34 @@
+package me.zhang.workbench.remoteViews;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import me.zhang.workbench.R;
+
+/**
+ * Created by Li on 8/18/2016 9:24 PM.
+ */
+public class MyAppWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int appWidgetId : appWidgetIds) {
+            // Create an Intent to launch WidgetActivity
+            Intent intent = new Intent(context, WidgetActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_provider_layout);
+            views.setOnClickPendingIntent(R.id.fire_button, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
