dafe929ae71d472e00f98f50305bbb955a81a655
zhangxiangdong <zxdeinstein@163.com>
2016-09-23 17:02:36 +0800

    Add ConstraintLayout

---
+++ Workbench/src/main/java/me/zhang/workbench/layout/ConstraintActivity.java
@@ -0,0 +1,15 @@
+package me.zhang.workbench.layout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.zhang.workbench.R;
+
+public class ConstraintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_0000);
+    }
+}
