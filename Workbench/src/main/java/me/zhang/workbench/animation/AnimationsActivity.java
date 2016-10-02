e495890d692a77c79bf4533a7c47218f2549a5e3
zhangxiangdong <zxdeinstein@163.com>
2016-10-02 18:08:57 +0800

    Demonstrate custom animation

--- Workbench/src/main/java/me/zhang/workbench/animation/AnimationsActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/animation/AnimationsActivity.java
@@ -12,19 +12,27 @@ import me.zhang.workbench.R;
 public class AnimationsActivity extends AppCompatActivity {
 
     private Animation mAnimations;
+    private Animation mRotate3dAnimation;
+
     private ImageView mTargetImage;
+    private ImageView mRotate3dImgae;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animations);
         mTargetImage = (ImageView) findViewById(R.id.animTargetImage);
+        mRotate3dImgae = (ImageView) findViewById(R.id.rorate3dImage);
 
         mAnimations = AnimationUtils.loadAnimation(this, R.anim.animations);
+        mRotate3dAnimation = new Rotate3dAnimation(0, 360, 0, 0, 0, true);
+        mRotate3dAnimation.setDuration(1600);
+        mRotate3dAnimation.setRepeatCount(Animation.INFINITE);
     }
 
     public void startAnimations(View view) {
         mTargetImage.startAnimation(mAnimations);
+        mRotate3dImgae.startAnimation(mRotate3dAnimation);
         view.setEnabled(false);
     }
 }
