a6a554909e84dc7e3892cef4c905510e4006e6c0
zhangxiangdong <zxdeinstein@163.com>
2016-10-15 11:21:24 +0800

    Compare FAB with Button

--- Workbench/src/main/java/me/zhang/workbench/design/FabActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/FabActivity.java
@@ -1,9 +1,7 @@
 package me.zhang.workbench.design;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import me.zhang.workbench.R;
 
@@ -15,7 +13,4 @@ public class FabActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fab);
     }
 
-    public void fab(View view) {
-        Snackbar.make(findViewById(R.id.activity_fab), "Floating", Snackbar.LENGTH_LONG).show();
-    }
 }
