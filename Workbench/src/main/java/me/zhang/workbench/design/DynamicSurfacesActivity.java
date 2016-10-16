bdee7c991edc3508528592dc712a5e8c7506a294
zhangxiangdong <zxdeinstein@163.com>
2016-10-16 16:29:58 +0800

    Activity 切换效果

--- Workbench/src/main/java/me/zhang/workbench/design/DynamicSurfacesActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/DynamicSurfacesActivity.java
@@ -9,7 +9,15 @@ public class DynamicSurfacesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(R.anim.right_in, R.anim.left_out);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_surfaces);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.left_in, R.anim.right_out);
+    }
+
 }
