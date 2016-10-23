d6ce91ebe74ffaa1e14511658b49016c2e773583
zhangxiangdong <zxdeinstein@163.com>
2016-10-23 12:55:21 +0800

    Create aspect ratio ImageView

---
+++ Workbench/src/main/java/me/zhang/workbench/design/image/ThreeTwoImageActivity.java
@@ -0,0 +1,15 @@
+package me.zhang.workbench.design.image;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.zhang.workbench.R;
+
+public class ThreeTwoImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_two_image);
+    }
+}
