a9941c390a80633766538c34a429e6d93139b282
zhangxiangdong <zxdeinstein@163.com>
2016-10-26 21:51:34 +0800

    Android 4: ViewPropertyAnimator

--- Workbench/src/main/java/me/zhang/workbench/design/animation/AnimateActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/animation/AnimateActivity.java
@@ -4,55 +4,79 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import me.zhang.workbench.R;
 
-public class AnimateActivity extends AppCompatActivity implements View.OnClickListener {
+public class AnimateActivity extends AppCompatActivity {
 
-    private Button mButton;
+    @InjectView(R.id.animXmlButton)
+    Button mXmlButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animate);
 
-        mButton = (Button) findViewById(R.id.animButton);
-        mButton.setOnClickListener(this);
-    }
+        ButterKnife.inject(this);
 
-    @Override
-    public void onClick(View v) {
-        Animation animationSet = AnimationUtils.loadAnimation(this, R.anim.slideoutbutton);
-        animationSet.setAnimationListener(new Animation.AnimationListener() {
+        mXmlButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                mButton.setVisibility(View.GONE);
-
-                startActivity(new Intent(AnimateActivity.this, TargetActivity.class));
-                overridePendingTransition(R.anim.bottom_in, R.anim.top_out);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
+            public void onClick(View v) {
+                Animation animationSet = AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.slideoutbutton);
+                animationSet.setAnimationListener(new AnimationListenerStub() {
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        mXmlButton.setVisibility(View.GONE);
+                        startNewActivity();
+                    }
+                });
+                mXmlButton.startAnimation(animationSet);
             }
         });
 
-        mButton.startAnimation(animationSet);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mButton.setVisibility(View.VISIBLE);
+    public void animCode(final View view) {
+        AnimationSet animationSet = new AnimationSet(this, null);
+        animationSet.addAnimation(new AlphaAnimation(1f, 0f));
+        animationSet.setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime));
+        animationSet.addAnimation(new TranslateAnimation(0, 0, 0, -view.getHeight()));
+        animationSet.setAnimationListener(new AnimationListenerStub() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.setVisibility(View.GONE);
+                startNewActivity();
+            }
+        });
+        view.startAnimation(animationSet);
+    }
+
+    public void animProp(final View view) {
+        view.animate()
+                .alpha(0f)
+                .translationY(-view.getHeight())
+                .setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime))
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.setVisibility(View.GONE);
+                        startNewActivity();
+                    }
+                });
+    }
+
+    private void startNewActivity() {
+        startActivity(new Intent(AnimateActivity.this, TargetActivity.class));
+        overridePendingTransition(R.anim.bottom_in, R.anim.top_out);
     }
 
 }
---
+++ Workbench/src/main/java/me/zhang/workbench/design/animation/AnimationListenerStub.java
@@ -0,0 +1,23 @@
+package me.zhang.workbench.design.animation;
+
+import android.view.animation.Animation;
+
+/**
+ * Created by Li on 2016/10/26 21:46.
+ */
+public class AnimationListenerStub implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
