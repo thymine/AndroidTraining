0ba9b85c3f02d801e619f24d9fdd2eaba40e7b43
zhangxiangdong <zxdeinstein@163.com>
2016-10-29 17:58:55 +0800

    Define enter transition

--- Workbench/src/main/java/me/zhang/workbench/design/transition/DummyActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/transition/DummyActivity.java
@@ -2,6 +2,9 @@ package me.zhang.workbench.design.transition;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Slide;
+import android.view.Gravity;
+import android.view.animation.AnimationUtils;
 
 import me.zhang.workbench.R;
 
@@ -10,6 +13,13 @@ public class DummyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Slide slide = new Slide(Gravity.BOTTOM);
+        slide.addTarget(R.id.description);
+        slide.setInterpolator(AnimationUtils.loadInterpolator(this, android.R.interpolator.linear_out_slow_in));
+        slide.setDuration(getResources().getInteger(android.R.integer.config_shortAnimTime));
+
+        getWindow().setEnterTransition(slide);
+
         setContentView(R.layout.activity_dummy);
     }
 }
