418f42b7e083e44bb056cd323b61df57ecbb2f82
zhangxiangdong <zxdeinstein@163.com>
2016-10-30 11:24:05 +0800

    Cross to Tick

--- Workbench/src/main/java/me/zhang/workbench/design/vector/VectorDrawableActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/vector/VectorDrawableActivity.java
@@ -14,6 +14,11 @@ public class VectorDrawableActivity extends AppCompatActivity {
     private AnimatedVectorDrawable fillHeart;
     private boolean full = false;
 
+    private ImageView tickCross;
+    private AnimatedVectorDrawable tickToCross;
+    private AnimatedVectorDrawable crossToTick;
+    private boolean tick = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +26,24 @@ public class VectorDrawableActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.image_view);
         emptyHeart = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_heart_empty);
         fillHeart = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_heart_fill);
+
+        tickCross = (ImageView) findViewById(R.id.tick_cross);
+        tickToCross = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_tick_to_cross);
+        crossToTick = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_cross_to_tick);
     }
 
-    public void animate(View view) {
+    public void animateHeart(View view) {
         AnimatedVectorDrawable drawable = full ? emptyHeart : fillHeart;
         imageView.setImageDrawable(drawable);
         drawable.start();
         full = !full;
     }
+
+    public void animateTickCross(View view) {
+        AnimatedVectorDrawable drawable = tick ? tickToCross : crossToTick;
+        tickCross.setImageDrawable(drawable);
+        drawable.start();
+        tick = !tick;
+    }
+
 }
