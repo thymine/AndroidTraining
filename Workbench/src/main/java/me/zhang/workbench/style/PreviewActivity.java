b306b234a03a3a1b1eacda8d5bee84654a9a4403
zhangxiangdong <zxdeinstein@163.com>
2016-11-04 10:35:19 +0800

    Use the material theme

--- Workbench/src/main/java/me/zhang/workbench/style/PreviewActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/style/PreviewActivity.java
@@ -1,11 +1,11 @@
 package me.zhang.workbench.style;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import me.zhang.workbench.R;
 
-public class PreviewActivity extends AppCompatActivity {
+public class PreviewActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
