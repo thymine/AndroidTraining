84d9e507a34e476212bd39d99e198c9a28f1bdf6
zhangxiangdong <zxdeinstein@163.com>
2016-11-06 21:54:57 +0800

    Leak activity

--- Workbench/src/main/java/me/zhang/workbench/memory/MemoryManagement.java
+++ Workbench/src/main/java/me/zhang/workbench/memory/MemoryManagement.java
@@ -1,8 +1,11 @@
 package me.zhang.workbench.memory;
 
 import android.app.ActivityManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -14,13 +17,22 @@ public class MemoryManagement extends AppCompatActivity {
     @InjectView(R.id.heapSizeText)
     TextView mHeapSizeText;
 
-    @InjectView(R.id.leageHeapSizeText)
+    @InjectView(R.id.largeHeapSizeText)
     TextView mLargeHeapSizeText;
+
+    @InjectView(R.id.recycledImage)
+    ImageView mRecycledImage;
+
+    static Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_management);
+
+        if (mBitmap == null) {
+            mBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.large_bitmap);
+        }
 
         ButterKnife.inject(this);
 
@@ -28,5 +40,6 @@ public class MemoryManagement extends AppCompatActivity {
         mHeapSizeText.setText(getString(R.string.heap_size, manager.getMemoryClass()));
 
         mLargeHeapSizeText.setText(getString(R.string.large_heap_size, manager.getLargeMemoryClass()));
+        mRecycledImage.setImageBitmap(mBitmap);
     }
 }
