e00b2ee1b385a3196867e6632082681fd9d0b0f8
zhangxiangdong <zxdeinstein@163.com>
2016-11-13 22:36:50 +0800

    Enable DayNight theme

--- Workbench/src/main/java/me/zhang/workbench/MainApp.java
+++ Workbench/src/main/java/me/zhang/workbench/MainApp.java
@@ -1,6 +1,9 @@
 package me.zhang.workbench;
 
 import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+import static android.support.v7.app.AppCompatDelegate.MODE_NIGHT_AUTO;
 
 /**
  * Created by Li on 6/16/2016 9:37 PM.
@@ -8,6 +11,10 @@ import android.app.Application;
 public class MainApp extends Application {
 
     public static Application me;
+
+    static {
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_AUTO);
+    }
 
     @Override
     public void onCreate() {
