688a46ae75966b928d7d5830ae53352184005138
zhangxiangdong <zxdeinstein@163.com>
2016-11-26 09:05:40 +0800

    保存和恢复 Activity 状态

--- Workbench/src/main/java/me/zhang/workbench/state/RestoreActivityState.java
+++ Workbench/src/main/java/me/zhang/workbench/state/RestoreActivityState.java
@@ -2,14 +2,52 @@ package me.zhang.workbench.state;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import me.zhang.workbench.R;
 
 public class RestoreActivityState extends AppCompatActivity {
+
+    public static final String INPUT_TEXT = "new_text";
+    @InjectView(R.id.new_edittext)
+    EditText mNewEditText;
+
+    private String mInputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restore_state);
+        ButterKnife.inject(this);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            mInputText = savedInstanceState.getString(INPUT_TEXT);
+            showInputText();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(INPUT_TEXT, mInputText);
+
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(outState);
+    }
+
+    public void inputText(View view) {
+        mInputText = mNewEditText.getText().toString();
+        showInputText();
+    }
+
+    private void showInputText() {
+        Toast.makeText(this, mInputText, Toast.LENGTH_SHORT).show();
     }
 }
