507c044a04ae21f94585075d903f15166b87d7ab
zhangxiangdong <zxdeinstein@163.com>
2016-12-03 21:23:02 +0800

    Use BuildConfig

--- GradleForAndroid/src/main/java/me/zhang/gradleforandroid/MainActivity.java
+++ GradleForAndroid/src/main/java/me/zhang/gradleforandroid/MainActivity.java
@@ -1,13 +1,23 @@
 package me.zhang.gradleforandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.author)
+    TextView mAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        mAuthor.setText(BuildConfig.AUTHOR);
     }
 }
