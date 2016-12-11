2dd52251b6b7e5590e35e4174535321d6e56f483
zhangxiangdong <zxdeinstein@163.com>
2016-12-11 13:13:53 +0800

    Build Debuggable Release

---
+++ GradleForAndroid/src/debug/java/me.zhang.gradleforandroid/http/HttpApi.java
@@ -0,0 +1,10 @@
+package me.zhang.gradleforandroid.http;
+
+/**
+ * Created by Zhang on 12/11/2016 12:53 PM.
+ */
+public class HttpApi {
+
+    public static final String SERVER = "http://www.yahoo.com/";
+
+}
