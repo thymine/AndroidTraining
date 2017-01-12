8ce4180500f1fe277ef1e8ce51c1f7fcb2959e71
zhangxiangdong <zxdeinstein@163.com>
2017-01-12 18:06:27 +0800

    Draw triangle

--- Workbench/src/main/java/me/zhang/workbench/graphics/OpenGLES20Activity.java
+++ Workbench/src/main/java/me/zhang/workbench/graphics/OpenGLES20Activity.java
@@ -1,13 +1,9 @@
 package me.zhang.workbench.graphics;
 
 import android.content.Context;
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
 public class OpenGLES20Activity extends AppCompatActivity {
 
@@ -41,27 +37,6 @@ public class OpenGLES20Activity extends AppCompatActivity {
             // Render the view only when there is a change in the drawing data
             setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         }
-    }
-
-    class MyGLRenderer implements GLSurfaceView.Renderer {
-
-        @Override
-        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-            // Set the background frame color
-            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        }
-
-        @Override
-        public void onSurfaceChanged(GL10 gl, int width, int height) {
-            GLES20.glViewport(0, 0, width, height);
-        }
-
-        @Override
-        public void onDrawFrame(GL10 gl) {
-            // Redraw background color
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        }
-
     }
 
 }
