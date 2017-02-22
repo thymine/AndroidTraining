02549246589d7738ef162a0e3c57b9eb8077f912
zhangxiangdong <zxdeinstein@163.com>
2017-02-22 10:00:08 +0800

    Refactor

--- Workbench/src/main/java/me/zhang/workbench/launchMode/BetaActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/launchMode/BetaActivity.java
@@ -1,9 +1,21 @@
 package me.zhang.workbench.launchMode;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class BetaActivity extends BaseActivity {
+import me.zhang.workbench.R;
+
+public class BetaActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beta);
+        findViewById(R.id.nextActivity).setOnClickListener(this);
+    }
 
     @Override
     public void onClick(View v) {
