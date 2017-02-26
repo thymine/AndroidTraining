ca25fa84d6f3a8b7a13f61f5b8d159bc35ce7432
zhangxiangdong <zxdeinstein@163.com>
2017-02-26 10:32:12 +0800

    Test ThreadLocal

---
+++ Workbench/src/main/java/me/zhang/workbench/thread/ThreadActivity.java
@@ -0,0 +1,26 @@
+package me.zhang.workbench.thread;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import me.zhang.workbench.R;
+
+public class ThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+
+    public void onClickButton(View view) {
+        switch (view.getId()) {
+            case R.id.threadLocal:
+                startActivity(new Intent(this, ThreadLocalActivity.class));
+                break;
+        }
+    }
+
+}
