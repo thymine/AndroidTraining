4d697b0d27d55b2ae4dd2f2855b5789810b7c161
zhangxiangdong <zxdeinstein@163.com>
2017-02-27 15:34:02 +0800

    Test with Robolectric

--- GradleForAndroid/src/main/java/me/zhang/gradleforandroid/MainActivity.java
+++ GradleForAndroid/src/main/java/me/zhang/gradleforandroid/MainActivity.java
@@ -1,13 +1,17 @@
 package me.zhang.gradleforandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.zhang.gradleforandroid.http.ContentGrabber;
 import me.zhang.gradleforandroid.http.HttpApi;
 
@@ -28,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.contents)
     TextView mContents;
 
+    @BindView(R.id.login)
+    Button mLoginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,4 +50,10 @@ public class MainActivity extends AppCompatActivity {
         Grabber contentGrabber = new ContentGrabber();
         mContents.setText(contentGrabber.grab());
     }
+
+    @OnClick(R.id.login)
+    public void login(View view) { // All arguments to the listener method are optional.
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
 }
