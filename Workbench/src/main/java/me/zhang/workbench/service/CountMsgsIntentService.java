2521c46a85615eb9489e81ab00f41bd0492316c2
zhangxiangdong <zxdeinstein@163.com>
2017-05-08 15:54:41 +0800

    Check permission then start IntentService

---
+++ Workbench/src/main/java/me/zhang/workbench/service/CountMsgsIntentService.java
@@ -0,0 +1,48 @@
+package me.zhang.workbench.service;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Telephony;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by zhangxiangdong on 2017/5/8.
+ */
+public class CountMsgsIntentService extends IntentService {
+
+    public static final String NUMBER_KEY = "number";
+    private static final String TAG = CountMsgsIntentService.class.getSimpleName();
+
+    public CountMsgsIntentService() {
+        super("CountMsgsIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent == null) return;
+
+        String phoneNumber = intent.getStringExtra(NUMBER_KEY);
+        Cursor cursor = getMsgsFrom(phoneNumber);
+        int numberOfMsgs = cursor.getCount();
+        Log.i(TAG, "onHandleIntent: numberOfMsgs -> " + numberOfMsgs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private Cursor getMsgsFrom(String phoneNumber) {
+        String[] projection = {
+                Telephony.Sms._ID,
+                Telephony.Sms.ADDRESS,
+                Telephony.Sms.BODY,
+        };
+
+        String whereClause = Telephony.Sms.ADDRESS + " = '" + phoneNumber + "'";
+        Uri quri = Uri.parse("content://sms/inbox");
+
+        return getContentResolver().query(quri, projection, whereClause, null, null);
+    }
+}
