8b4f4d48bfeba2fbcbd38151cbb7850d049bcbd2
zhangxiangdong <zxdeinstein@163.com>
2017-05-09 15:12:19 +0800

    Post result with PendingIntent

--- Workbench/src/main/java/me/zhang/workbench/service/CountMsgsIntentService.java
+++ Workbench/src/main/java/me/zhang/workbench/service/CountMsgsIntentService.java
@@ -2,6 +2,7 @@ package me.zhang.workbench.service;
 
 import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -9,6 +10,10 @@ import android.os.Build;
 import android.provider.Telephony;
 import android.support.annotation.Nullable;
 import android.util.Log;
+
+import static me.zhang.workbench.thread.IntentServiceActivity.PENDING_RESULT;
+import static me.zhang.workbench.thread.IntentServiceActivity.RESULT;
+import static me.zhang.workbench.thread.IntentServiceActivity.RESULT_CODE;
 
 /**
  * Created by zhangxiangdong on 2017/5/8.
@@ -30,6 +35,16 @@ public class CountMsgsIntentService extends IntentService {
         Cursor cursor = getMsgsFrom(phoneNumber);
         int numberOfMsgs = cursor.getCount();
         Log.i(TAG, "onHandleIntent: numberOfMsgs -> " + numberOfMsgs);
+
+        PendingIntent reply = intent.getParcelableExtra(PENDING_RESULT);
+        Intent result = new Intent();
+        result.putExtra(RESULT, numberOfMsgs);
+        try {
+            reply.send(this, RESULT_CODE, result);
+        } catch (PendingIntent.CanceledException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
