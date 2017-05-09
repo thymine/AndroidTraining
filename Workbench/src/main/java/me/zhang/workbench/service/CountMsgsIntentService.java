4840029fafba5a5aa965703483ddfb7f3982c4ab
zhangxiangdong <zxdeinstein@163.com>
2017-05-09 15:37:44 +0800

    Post result as system Notifications

--- Workbench/src/main/java/me/zhang/workbench/service/CountMsgsIntentService.java
+++ Workbench/src/main/java/me/zhang/workbench/service/CountMsgsIntentService.java
@@ -2,6 +2,7 @@ package me.zhang.workbench.service;
 
 import android.annotation.TargetApi;
 import android.app.IntentService;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,7 +10,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.Telephony;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import me.zhang.workbench.R;
 
 import static me.zhang.workbench.thread.IntentServiceActivity.PENDING_RESULT;
 import static me.zhang.workbench.thread.IntentServiceActivity.RESULT;
@@ -45,6 +49,21 @@ public class CountMsgsIntentService extends IntentService {
             e.printStackTrace();
         }
 
+        notifyUser(phoneNumber, numberOfMsgs);
+
+    }
+
+    private void notifyUser(String phoneNumber, int msgsCount) {
+        String msg = String.format("Found %d from the phone number %s", msgsCount, phoneNumber);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_sms_counter_not)
+                        .setContentTitle("Inbox Counter")
+                        .setContentText(msg);
+        // Gets an instance of the NotificationManager service
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Sets an unique ID for this notification
+        nm.notify(phoneNumber.hashCode(), builder.build());
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
