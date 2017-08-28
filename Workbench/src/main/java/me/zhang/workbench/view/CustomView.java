e5bda6ea836e9a8ad92386237468d4304ecc8aff
zhangxiangdong <zxdeinstein@163.com>
2017-08-28 17:45:45 +0800

    Measure phase

--- Workbench/src/main/java/me/zhang/workbench/view/CustomView.java
+++ Workbench/src/main/java/me/zhang/workbench/view/CustomView.java
@@ -1,41 +1,45 @@
 package me.zhang.workbench.view;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
-import static android.graphics.Color.RED;
-import static me.zhang.workbench.utils.UiUtils.convertDpToPixel;
 
 /**
  * Created by Zhang on 2016/7/4 上午 9:34 .
  */
 public class CustomView extends View {
 
-    private Paint mPaint;
-    private String mHello = "Hello, world!";
-
-    public CustomView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
+    public CustomView(Context context) {
+        this(context, null);
     }
 
-    private void init(Context context) {
-        mPaint = new Paint();
-        mPaint.setTextSize(convertDpToPixel(18, context));
-        mPaint.setColor(RED);
+    public CustomView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(measureSquare(widthMeasureSpec), measureSquare(heightMeasureSpec));
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        // draw "Hello, world!" string here
-        canvas.drawText(mHello, getWidth() / 2 - mPaint.measureText(mHello) / 2, getHeight() / 2, mPaint);
+    private int measureSquare(int measureSpec) {
+        int result;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = 480;
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
     }
+
 }
