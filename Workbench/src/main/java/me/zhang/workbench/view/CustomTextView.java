bb441d0c9447620466c4dba105048aefe70d827f
zhangxiangdong <zxdeinstein@163.com>
2017-09-20 17:20:08 +0800

    Shimmer text

--- Workbench/src/main/java/me/zhang/workbench/view/CustomTextView.java
+++ Workbench/src/main/java/me/zhang/workbench/view/CustomTextView.java
@@ -2,8 +2,12 @@ package me.zhang.workbench.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatTextView;
@@ -19,6 +23,10 @@ public class CustomTextView extends AppCompatTextView {
     private Paint mBorderPaint;
     private Paint mBackgroundPaint;
     private RectF mRectF = new RectF();
+    private int mViewWidth;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private int mTranslate;
 
     public CustomTextView(Context context) {
         this(context, null);
@@ -50,5 +58,31 @@ public class CustomTextView extends AppCompatTextView {
         super.onDraw(canvas);
 
         canvas.drawRect(mRectF, mBorderPaint);
+
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mLinearGradient = new LinearGradient(
+                        0, 0, mViewWidth, 0, new int[]{Color.BLUE, 0xffffffff, Color.BLUE}, null,
+                        Shader.TileMode.CLAMP
+                );
+                getPaint().setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
     }
 }
