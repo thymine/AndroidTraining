617cb9c7797433deaa37c9c6fefa1b71b2f35bfa
zhangxiangdong <zxdeinstein@163.com>
2017-11-09 20:37:42 +0800

    Request JSON

--- Workbench/src/main/java/me/zhang/workbench/volley/VolleyTestActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/volley/VolleyTestActivity.java
@@ -9,7 +9,10 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,11 +24,17 @@ public class VolleyTestActivity extends AppCompatActivity {
     @BindView(R.id.button_request)
     Button mRequestButton;
 
+    @BindView(R.id.button_request_json)
+    Button mRequestJsonButton;
+
     @BindView(R.id.text_response)
     TextView mResponseText;
 
     private static final String URL = "https://www.baidu.com/";
+    private static final String URL_JSON =
+            "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=7&video=1";
     private Request<String> mStringRequest;
+    private Request<JSONObject> mJSONObjectRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +65,31 @@ public class VolleyTestActivity extends AppCompatActivity {
         mStringRequest.setTag(this);
 
         RequestManager.INSTANCE.addToRequestQueue(mStringRequest);
+    }
+
+    @OnClick(R.id.button_request_json)
+    public void doRequestJson() {
+        if (mJSONObjectRequest != null) {
+            mJSONObjectRequest.cancel();
+        }
+
+        mJSONObjectRequest = new JsonObjectRequest(Request.Method.GET, URL_JSON, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mResponseText.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mResponseText.setText(R.string.response_error);
+                    }
+                }
+        );
+        mJSONObjectRequest.setTag(this);
+
+        RequestManager.INSTANCE.addToRequestQueue(mJSONObjectRequest);
     }
 
     @Override
