79bf58e563ce768273ead3cd15160e39e167c241
zhangxiangdong <zxdeinstein@163.com>
2017-11-10 10:20:46 +0800

    Write a Custom Request

---
+++ Workbench/src/main/java/me/zhang/workbench/volley/BingoResult.java
@@ -0,0 +1,12 @@
+package me.zhang.workbench.volley;
+
+import java.util.List;
+
+/**
+ * Created by zhangxiangdong on 2017/11/10.
+ */
+public class BingoResult {
+
+    public List<Image> images;
+
+}
