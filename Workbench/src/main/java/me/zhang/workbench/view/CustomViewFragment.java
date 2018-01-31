bec3e607fbf21f3ecb9b09c86a965dd4db474515
zhangxiangdong <zxdeinstein@163.com>
2018-01-31 17:15:16 +0800

    Draw into bitmap

--- Workbench/src/main/java/me/zhang/workbench/view/CustomViewFragment.java
+++ Workbench/src/main/java/me/zhang/workbench/view/CustomViewFragment.java
@@ -1,12 +1,18 @@
 package me.zhang.workbench.view;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.zhang.workbench.R;
 
@@ -16,6 +22,9 @@ import static me.zhang.workbench.view.CustomViewActivity.FRAGMENT_TITLE;
  * Created by Zhang on 8/19/2017 6:52 PM.
  */
 public class CustomViewFragment extends Fragment {
+
+    @BindView(R.id.custom_image)
+    ImageView mCustomImage;
 
     public static CustomViewFragment newInstance(String title) {
 
@@ -36,4 +45,30 @@ public class CustomViewFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final int width = 480;
+        final int height = 480;
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(bitmap);
+
+        int radius = 24;
+        final int cx = width / 2;
+        final int cy = height / 2;
+
+        /* draw circle */
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(cx, cy, radius, paint);
+
+        /* draw rectangle */
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.STROKE);
+        int offset = 60;
+        canvas.drawRect(cx - offset, cy - offset, cx + offset, cy + offset, paint);
+
+        mCustomImage.setImageBitmap(bitmap);
+    }
 }
