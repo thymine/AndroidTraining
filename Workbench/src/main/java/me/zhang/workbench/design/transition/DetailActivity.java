3c4442a46b7a7f64c9871dd3b5581b6ab7d1974f
zhangxiangdong <zxdeinstein@163.com>
2018-03-03 23:12:23 +0800

    A window into transitions

--- Workbench/src/main/java/me/zhang/workbench/design/transition/DetailActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/transition/DetailActivity.java
@@ -1,10 +1,8 @@
 package me.zhang.workbench.design.transition;
 
-import android.app.ActivityOptions;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import me.zhang.workbench.R;
 
@@ -13,13 +11,10 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.AppTheme_Base_Details_Light);
-
+        setTheme(R.style.AppThemeNoActionBar_Details);
         setContentView(R.layout.activity_detail);
-    }
 
-    public void exit(View view) {
-        Intent intent = new Intent(this, DummyActivity.class);
-        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
+        final int clickedColor = getIntent().getIntExtra(getString(R.string.key_clicked_color), Color.WHITE);
+        findViewById(R.id.v_clicked_color).setBackgroundColor(clickedColor);
     }
 }
--- Workbench/src/main/java/me/zhang/workbench/design/transition/GridActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/design/transition/GridActivity.java
@@ -1,24 +1,44 @@
 package me.zhang.workbench.design.transition;
 
-import android.app.ActivityOptions;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.zhang.workbench.R;
 
+import static android.app.ActivityOptions.makeSceneTransitionAnimation;
+import static android.graphics.Color.BLUE;
+import static android.graphics.Color.CYAN;
+import static android.graphics.Color.DKGRAY;
+import static android.graphics.Color.GRAY;
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.LTGRAY;
+import static android.graphics.Color.MAGENTA;
+import static android.graphics.Color.RED;
+import static android.graphics.Color.YELLOW;
+
 public class GridActivity extends AppCompatActivity {
 
+    private final int[] colors = {DKGRAY, GRAY, LTGRAY, RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA};
+    private final List<Integer> colorList = new ArrayList<>();
+
+    {
+        for (int i = 0; i < 30; i++) {
+            colorList.add(colors[(int) (Math.random() * colors.length)]);
+        }
+    }
+
     @BindView(R.id.grid)
-    GridView grid;
+    RecyclerView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,42 +46,43 @@ public class GridActivity extends AppCompatActivity {
         setContentView(R.layout.activity_grid);
         ButterKnife.bind(this);
 
+        grid.setHasFixedSize(true);
+        grid.addItemDecoration(new GridMarginDecoration(getResources().getDimensionPixelSize(R.dimen.grid_item_spacing)));
         grid.setAdapter(new GridAdapter());
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(GridActivity.this, DetailActivity.class),
-                        ActivityOptions.makeSceneTransitionAnimation(GridActivity.this
-                                , view, "hero"
-                        ).toBundle());
-            }
-        });
     }
 
-    private class GridAdapter extends BaseAdapter {
+    private class GridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         @Override
-        public int getCount() {
-            return 20;
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new VH(getLayoutInflater().inflate(R.layout.grid_item, parent, false));
         }
 
         @Override
-        public Object getItem(int position) {
-            return null;
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            holder.itemView.setBackgroundColor(colorList.get(position));
         }
 
         @Override
-        public long getItemId(int position) {
-            return position;
+        public int getItemCount() {
+            return colorList.size();
         }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = getLayoutInflater().inflate(R.layout.grid_item, parent, false);
+        class VH extends RecyclerView.ViewHolder {
+
+            public VH(View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @SuppressLint("NewApi")
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(GridActivity.this, DetailActivity.class);
+                        intent.putExtra(getString(R.string.key_clicked_color), colorList.get(getAdapterPosition()));
+                        startActivity(intent, makeSceneTransitionAnimation(GridActivity.this, v, getString(R.string.transition_name_hero)).toBundle());
+                    }
+                });
             }
-            ((TextView) convertView).setText("A");
-            return convertView;
         }
+
     }
 }
