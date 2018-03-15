da6eb3b6584062ab59f6a1034ccc4bcb08e70695
zhangxiangdong <zxdeinstein@163.com>
2018-03-15 18:23:25 +0800

    Transform coordinate

--- Workbench/src/main/java/me/zhang/workbench/ui/CircleViewActivity.java
+++ Workbench/src/main/java/me/zhang/workbench/ui/CircleViewActivity.java
@@ -11,19 +11,10 @@ import me.zhang.workbench.R;
  */
 public class CircleViewActivity extends AppCompatActivity {
 
-    private CircleMovingView circleMovingView;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle_view);
-        circleMovingView = findViewById(R.id.circleMovingView);
-        circleMovingView.start();
     }
 
-    @Override
-    protected void onDestroy() {
-        circleMovingView.stop();
-        super.onDestroy();
-    }
 }
