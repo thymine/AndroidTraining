75fac36d42da86e05a1bcbe4b07fc918e40a79e7
zhangxiangdong <zxdeinstein@163.com>
2018-03-18 11:42:07 +0800

    Draw on bitmap

---
+++ Workbench/src/main/java/me/zhang/workbench/ui/LineDrawingActivity.kt
@@ -0,0 +1,17 @@
+package me.zhang.workbench.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.zhang.workbench.R
+
+/**
+ * Created by Zhang on 3/18/2018 11:18 AM.
+ */
+class LineDrawingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_line_drawing)
+    }
+
+}
