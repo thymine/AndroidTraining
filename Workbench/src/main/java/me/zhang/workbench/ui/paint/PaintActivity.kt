35f58c2a666af763abc349d85a996e56aa731edc
zhangxiangdong <zxdeinstein@163.com>
2018-03-23 10:07:40 +0800

    Save drawing contents to local disk

--- Workbench/src/main/java/me/zhang/workbench/ui/paint/PaintActivity.kt
+++ Workbench/src/main/java/me/zhang/workbench/ui/paint/PaintActivity.kt
@@ -1,19 +1,27 @@
 package me.zhang.workbench.ui.paint
 
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.widget.Toast
 import com.flask.colorpicker.ColorPickerView
 import com.flask.colorpicker.builder.ColorPickerDialogBuilder
 import kotlinx.android.synthetic.main.activity_paint.*
 import me.zhang.workbench.R
 
-
 /**
  * Created by zhangxiangdong on 2018/3/20.
  */
 class PaintActivity : AppCompatActivity() {
+
+    companion object {
+        const val WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +47,8 @@ class PaintActivity : AppCompatActivity() {
                     .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
                     .density(12)
                     .setOnColorSelectedListener { selectedColor ->
-                        Toast.makeText(this, "0x" + Integer.toHexString(selectedColor), Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "0x" + Integer.toHexString(selectedColor),
+                                Toast.LENGTH_SHORT).show()
                     }
                     .setPositiveButton("Ok") { _, selectedColor, _ ->
                         changePaintColor(selectedColor)
@@ -53,6 +62,28 @@ class PaintActivity : AppCompatActivity() {
         clearButton.setOnClickListener { _ ->
             clearCanvas()
         }
+        captureButton.setOnClickListener { _ ->
+            // 检查读写权限
+            if (isStoragePermissionGranted()) {
+                captureCanvas()
+            } else {
+                // 请求读写权限
+                requestWriteExternalStoragePermission()
+            }
+        }
+    }
+
+    private fun requestWriteExternalStoragePermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE),
+                WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE)
+    }
+
+    private fun captureCanvas() {
+        var captureFilePath = paintView.captureCanvas()
+        if (TextUtils.isEmpty(captureFilePath)) {
+            captureFilePath = "Capture failed, see logs"
+        }
+        Toast.makeText(this, captureFilePath, Toast.LENGTH_SHORT).show()
     }
 
     private fun clearCanvas() {
@@ -61,6 +92,23 @@ class PaintActivity : AppCompatActivity() {
 
     private fun changePaintColor(selectedColor: Int) {
         paintView.changePaintColor(selectedColor)
+    }
+
+    private fun isStoragePermissionGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+        } else { // permission is automatically granted on sdk < 23 upon installation
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            captureCanvas()
+        }
     }
 
 }
