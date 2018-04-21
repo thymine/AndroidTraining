6d9715e7a0861ae590d047edc05ace4bc44c7164
zhangxiangdong <zxdeinstein@163.com>
2018-04-21 23:34:28 +0800

    Draw multiline text to Canvas

---
+++ Workbench/src/main/java/me/zhang/workbench/text/DrawTextActivity.kt
@@ -0,0 +1,13 @@
+package me.zhang.workbench.text
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.zhang.workbench.R
+
+class DrawTextActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_draw_text)
+    }
+}
