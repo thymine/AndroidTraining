134220ac26c0811b92cb25b07f5f4a1c3c917513
zhangxiangdong <zxdeinstein@163.com>
2018-04-26 16:10:17 +0800

    Draw circle image

---
+++ Workbench/src/main/java/me/zhang/workbench/ui/CirclePhotoView.kt
@@ -0,0 +1,59 @@
+package me.zhang.workbench.ui
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import me.zhang.workbench.R
+
+/**
+ * Created by zhangxiangdong on 2018/4/26 11:47.
+ */
+class CirclePhotoView : View {
+
+    private val catBitmapMatrix = Matrix()
+    private val circleMaskPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val catBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.cat)
+
+    private val circleMaskBitmap: Bitmap
+    private val circleMaskCanvas: Canvas
+    private val dstIn = PorterDuffXfermode(PorterDuff.Mode.DST_IN)
+
+    init {
+        val w = Math.min(catBitmap.width, catBitmap.height) / 2
+        circleMaskBitmap = Bitmap.createBitmap(w, w, Bitmap.Config.ARGB_8888)
+        circleMaskCanvas = Canvas(circleMaskBitmap)
+
+        val r = (w / 2).toFloat()
+        circleMaskCanvas.drawCircle(r, r, r, circleMaskPaint)
+    }
+
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        val circleMaskWidth = circleMaskBitmap.width * 1f
+        val catWidth = catBitmap.width * 1f / 2
+
+        val circleMaskLeft = (catWidth - circleMaskWidth) / 2f
+
+        @Suppress("DEPRECATION")
+        val layer = canvas.saveLayer(circleMaskLeft, 0f, circleMaskLeft + circleMaskWidth, circleMaskLeft + circleMaskWidth, null, Canvas.ALL_SAVE_FLAG)
+
+        catBitmapMatrix.setTranslate(0f, 0f)
+        catBitmapMatrix.postScale(0.5f, 0.5f)
+        canvas.drawBitmap(catBitmap, catBitmapMatrix, null)
+        circleMaskPaint.xfermode = dstIn
+        canvas.drawBitmap(circleMaskBitmap, circleMaskLeft, 0f, circleMaskPaint)
+
+        canvas.restoreToCount(layer)
+    }
+
+}
