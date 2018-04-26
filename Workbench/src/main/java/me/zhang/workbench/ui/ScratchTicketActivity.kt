1202cf05afd503cca05d23233e85ba3e10929e02
zhangxiangdong <zxdeinstein@163.com>
2018-04-26 18:31:00 +0800

    Scratch ticket

---
+++ Workbench/src/main/java/me/zhang/workbench/ui/ScratchTicketActivity.kt
@@ -0,0 +1,13 @@
+package me.zhang.workbench.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.zhang.workbench.R
+
+class ScratchTicketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scratch_ticket)
+    }
+}
