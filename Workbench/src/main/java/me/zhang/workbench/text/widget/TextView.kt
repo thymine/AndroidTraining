f01425fc346188f7c6d7fcd861b08024f05aca79
zhangxiangdong <zxdeinstein@163.com>
2018-04-28 14:11:43 +0800

    Measure your view

--- Workbench/src/main/java/me/zhang/workbench/text/widget/TextView.kt
+++ Workbench/src/main/java/me/zhang/workbench/text/widget/TextView.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import me.zhang.workbench.R
 import me.zhang.workbench.utils.dp
-import me.zhang.workbench.x.drawMultilineText
 
 /**
  * Created by Zhang on 2018/4/21 22:45.
  */
 class TextView : View {
 
-    private val text: CharSequence
+    private val text: String
     private val textPaint = TextPaint()
+    private val textRect = Rect()
 
     init {
         textPaint.flags = Paint.ANTI_ALIAS_FLAG
@@ -29,7 +30,7 @@ class TextView : View {
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         val a = context.obtainStyledAttributes(attrs, R.styleable.TextView)
 
-        text = a.getText(R.styleable.TextView_text)
+        text = a.getString(R.styleable.TextView_text)
 
         val textColor = a.getColor(R.styleable.TextView_textColor, Color.BLACK)
         textPaint.color = textColor
@@ -39,19 +40,64 @@ class TextView : View {
         val textSize = a.getDimension(R.styleable.TextView_textSize, 14.dp())
         textPaint.textSize = textSize
 
+        textPaint.getTextBounds(text, 0, text.length, textRect)
+
         a.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = measureWidth(widthMeasureSpec, textRect.width() + paddingLeft + paddingRight)
+        val height = measureHeight(heightMeasureSpec, textRect.height() + paddingTop + paddingBottom)
+        setMeasuredDimension(width, height)
+    }
+
+    private fun measureWidth(widthMeasureSpec: Int, textWidth: Int): Int {
+        val size = MeasureSpec.getSize(widthMeasureSpec)
+        val mode = MeasureSpec.getMode(widthMeasureSpec)
+        var width = 0
+        when (mode) {
+            MeasureSpec.EXACTLY -> {
+                // 宽度为match_parent或者具体数值
+                width = size
+            }
+            MeasureSpec.AT_MOST -> {
+                width = textWidth
+            }
+            MeasureSpec.UNSPECIFIED -> {
+
+            }
+        }
+        return width
+    }
+
+    private fun measureHeight(heightMeasureSpec: Int, textHeight: Int): Int {
+        val size = MeasureSpec.getSize(heightMeasureSpec)
+        val mode = MeasureSpec.getMode(heightMeasureSpec)
+        var height = 0
+        when (mode) {
+            MeasureSpec.EXACTLY -> {
+                height = size
+            }
+            MeasureSpec.AT_MOST -> {
+                height = textHeight
+            }
+            MeasureSpec.UNSPECIFIED -> {
+
+            }
+        }
+        return height
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        // 水平居中绘制文本
+        val x = (measuredWidth - textRect.width()) / 2f
 
-        canvas.drawMultilineText(
-                text,
-                textPaint,
-                (measuredWidth - paddingLeft - paddingRight),
-                x + paddingLeft,
-                y + paddingTop
-        )
+        // 垂直居中绘制文本
+        // 获取baseline在y方向的坐标值
+        val y = measuredHeight / 2 + (textPaint.descent() - textPaint.ascent()) / 2 - textPaint.descent()
+
+        canvas.drawText(text, x, y, textPaint)
     }
 
 }
