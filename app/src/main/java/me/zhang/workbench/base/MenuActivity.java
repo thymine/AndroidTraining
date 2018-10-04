1ea8b40e20a716807447d5b5c055da14211d43d3
zhangxiangdong <zxdeinstein@163.com>
2018-10-04 12:51:45 +0800

    Add entry click history

--- app/src/main/java/me/zhang/workbench/base/MenuActivity.java
+++ app/src/main/java/me/zhang/workbench/base/MenuActivity.java
@@ -4,8 +4,10 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
@@ -15,6 +17,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -24,17 +36,57 @@ import me.zhang.workbench.R;
  * Created by Zhang on 5/11/2015 14:20 下午.
  */
 public abstract class MenuActivity extends AppCompatActivity
-        implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
+        implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener, HistoryAdapter.OnHistoryItemClickListener {
 
     private static final String TAG = MenuActivity.class.getSimpleName();
+    public static final String KEY_HISTORIES = "key_histories";
+    private static final String NAME_HISTORIES = "name_histories";
+
     private ArrayAdapter<String> mAdapter;
+    private final List<String> historyKeys = new ArrayList<>();
 
     private SortedMap<String, Intent> actions = new TreeMap<>();
+    private HistoryAdapter adapter;
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         String key = (String) parent.getItemAtPosition(position);
+        onClickItem(key);
+    }
+
+    private void onClickItem(String key) {
+        saveClickedKey(key);
         startActivity(actions.get(key));
+    }
+
+    private void saveClickedKey(String key) {
+        historyKeys.remove(key);
+        historyKeys.add(0, key);
+        saveHistoryKeys();
+    }
+
+    void saveHistoryKeys() {
+        String histories = new Gson().toJson(historyKeys.subList(0, 10)); // 最多保存10个历史记录
+        SharedPreferences.Editor editor = getSharedPreferences(NAME_HISTORIES, MODE_PRIVATE).edit();
+        editor.putString(KEY_HISTORIES, histories);
+        editor.apply();
+    }
+
+    void restoreHistoryKeys() {
+        SharedPreferences prefs = getSharedPreferences(NAME_HISTORIES, MODE_PRIVATE);
+        String restoredHistories = prefs.getString(KEY_HISTORIES, null);
+        List<String> historyList = new Gson().fromJson(restoredHistories, new TypeToken<List<String>>() {
+        }.getType());
+
+        if (historyList != null) {
+            historyKeys.clear();
+            historyKeys.addAll(historyList.subList(0, 10)); // 最多展示10个历史记录
+            notifyHistoriesChanged();
+        }
+    }
+
+    private void notifyHistoriesChanged() {
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -42,7 +94,16 @@ public abstract class MenuActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_menu);
-        ListView list = (ListView) findViewById(android.R.id.list);
+
+        RecyclerView historyList = findViewById(R.id.historyList);
+        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
+        layoutManager.setFlexDirection(FlexDirection.ROW);
+        layoutManager.setJustifyContent(JustifyContent.FLEX_START);
+        historyList.setLayoutManager(layoutManager);
+        adapter = new HistoryAdapter(historyKeys, this);
+        historyList.setAdapter(adapter);
+
+        ListView list = findViewById(android.R.id.list);
         list.setEmptyView(findViewById(android.R.id.empty));
         list.setOnItemClickListener(this);
         list.setFastScrollEnabled(true);
@@ -51,11 +112,18 @@ public abstract class MenuActivity extends AppCompatActivity
 
         prepareMenu();
 
+        //noinspection ToArrayCallWithZeroLengthArrayArgument
         String[] keys = actions.keySet().toArray(new String[actions.keySet().size()]);
 
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, keys);
         list.setAdapter(mAdapter);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        restoreHistoryKeys();
     }
 
     @Override
@@ -69,6 +137,7 @@ public abstract class MenuActivity extends AppCompatActivity
 
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        //noinspection ConstantConditions
         SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
         searchView.setSearchableInfo(searchableInfo);
 
@@ -93,4 +162,8 @@ public abstract class MenuActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public void onClick(@NotNull String historyKey) {
+        onClickItem(historyKey);
+    }
 }
