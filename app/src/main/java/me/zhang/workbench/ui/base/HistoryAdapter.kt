c51685b40256392b564ff2fb7ac7161f1464a3e2
zhangxiangdong <zxdeinstein@163.com>
2018-10-06 11:18:08 +0800

    Implement drag and drop

--- app/src/main/java/me/zhang/workbench/ui/base/HistoryAdapter.kt
+++ app/src/main/java/me/zhang/workbench/ui/base/HistoryAdapter.kt
@@ -1,7 +1,8 @@
 package me.zhang.workbench.ui.base
 
-import androidx.recyclerview.widget.RecyclerView
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -11,6 +12,7 @@ class HistoryAdapter(private val histories: List<String>, private val clickListe
 
     interface OnHistoryItemClickListener {
         fun onClick(history: String)
+        fun onLongClick(v: View, touchX: Float, touchY: Float)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
@@ -29,13 +31,30 @@ class HistoryAdapter(private val histories: List<String>, private val clickListe
         holder.bind(getItem(position))
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     class VH(itemView: View, private val clickListener: OnHistoryItemClickListener) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView) {
 
-        private val historyItem = itemView.findViewById<TextView>(R.id.historyItem)
+        private val historyItem: TextView = itemView.findViewById(R.id.historyItem)
+        private val lastTouchDownXY = FloatArray(2)
+
+        init {
+            this.historyItem.setOnTouchListener { _, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    lastTouchDownXY[0] = event.x
+                    lastTouchDownXY[1] = event.y
+                }
+                false
+            }
+        }
 
         fun bind(item: String) {
             historyItem.text = item
             historyItem.setOnClickListener { clickListener.onClick(item) }
+            historyItem.setOnLongClickListener { v ->
+                v.tag = item
+                clickListener.onLongClick(v, lastTouchDownXY[0], lastTouchDownXY[1])
+                true
+            }
         }
 
     }
