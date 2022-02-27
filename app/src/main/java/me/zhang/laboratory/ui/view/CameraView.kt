40b08360fa76ee72d6d87a8190d1541d2c20029f
zhangxiangdong <418033+zhangxiangdong28@user.noreply.gitee.com>
2022-02-27 16:40:12 +0800

    Set camera location

--- app/src/main/java/me/zhang/laboratory/ui/view/CameraView.kt
+++ app/src/main/java/me/zhang/laboratory/ui/view/CameraView.kt
@@ -3,10 +3,16 @@ package me.zhang.laboratory.ui.view
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import me.zhang.laboratory.R
+import java.util.*
 
 class CameraView : AppCompatImageView {
+
+    companion object {
+        const val TAG = "CameraView"
+    }
 
     private val camera: Camera = Camera()
     private val m: Matrix = Matrix()
@@ -36,20 +42,56 @@ class CameraView : AppCompatImageView {
 
         canvas?.save()
 
+        //region CAMERA
         camera.save()
+        // onDraw: camera(0.000000, 0.000000, -8.000000)
+        // -8 * 72 = -578px
+        Log.d(
+            TAG,
+            String.format(
+                Locale.getDefault(),
+                "onDraw: camera(%f, %f, %f)",
+                camera.locationX,
+                camera.locationY,
+                camera.locationZ
+            )
+        )
         camera.rotate(rotateX, rotateY, rotateZ)
-        val centerX = measuredWidth / 2.toFloat()
-        val centerY = measuredHeight / 2.toFloat()
-        camera.getMatrix(m)
-        m.preTranslate(-centerX, -centerY)
-        m.postTranslate(centerX, centerY)
+
+        toImageCenterC()
+
+//        toImageCenterM()
+
         camera.restore()
+        //endregion
 
         canvas?.setMatrix(m)
 
         super.onDraw(canvas)
 
         canvas?.restore()
+    }
+
+    private fun toImageCenterC() {
+        val centerX = measuredWidth / 2.toFloat()
+        val centerY = measuredHeight / 2.toFloat()
+
+        val centerCX = centerX / 72
+        val centerCY = centerY / 72
+
+        camera.setLocation(centerCX, -centerCY, camera.locationZ)
+
+        camera.getMatrix(m)
+    }
+
+    private fun toImageCenterM() {
+        val centerX = measuredWidth / 2.toFloat()
+        val centerY = measuredHeight / 2.toFloat()
+
+        camera.getMatrix(m)
+
+        m.preTranslate(-centerX, -centerY)
+        m.postTranslate(centerX, centerY)
     }
 
     fun setRotateX(rotateX: Float) {
