8e3911fa5a39f978c7cd3958ce123e77046809db
zhangxiangdong <418033+zhangxiangdong28@user.noreply.gitee.com>
2022-09-05 17:03:24 +0800

    Dynamic factor

--- app/src/main/java/me/zhang/laboratory/ui/FoldActivity.kt
+++ app/src/main/java/me/zhang/laboratory/ui/FoldActivity.kt
@@ -1,12 +1,32 @@
 package me.zhang.laboratory.ui
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
-import me.zhang.laboratory.R
+import me.zhang.laboratory.databinding.ActivityFoldBinding
+import me.zhang.laboratory.ui.view.FoldView3
 
 class FoldActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFoldBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fold)
+        binding = ActivityFoldBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sbFactor.progress = (FoldView3.FACTOR * 100).toInt()
+        binding.sbFactor.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.fv.factor = progress * 1f / 100.toFloat()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
+
 }
--- app/src/main/java/me/zhang/laboratory/ui/view/FoldView3.kt
+++ app/src/main/java/me/zhang/laboratory/ui/view/FoldView3.kt
@@ -19,17 +19,31 @@ class FoldView3 : View {
 
     private val rawBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.img_read)
     private val bitmap: Bitmap = Bitmap.createScaledBitmap(
-            rawBitmap,
-            rawBitmap.width / 2,
-            rawBitmap.height / 2,
-            false
+        rawBitmap,
+        rawBitmap.width / 2,
+        rawBitmap.height / 2,
+        false
     )
     private val foldWidth = (bitmap.width / FOLD_COUNT).toFloat()
+    var factor: Float = FACTOR
+        set(value) {
+            field = value
+            setFoldMatrices()
+            invalidate()
+        }
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        val foldItemWidth = bitmap.width * FACTOR / FOLD_COUNT
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        setFoldMatrices()
+    }
+
+    private fun setFoldMatrices() {
+        val foldItemWidth = bitmap.width * factor / FOLD_COUNT
         val depth = sqrt(foldWidth * foldWidth - foldItemWidth * foldItemWidth) / 2
 
         for (i in 0 until FOLD_COUNT) {
@@ -41,17 +55,21 @@ class FoldView3 : View {
             val xEnd = foldWidth * (i + 1)
 
             val src = floatArrayOf(
-                    xStart, 0F,
-                    xEnd, 0F,
-                    xEnd, bitmap.height.toFloat(),
-                    xStart, bitmap.height.toFloat()
+                xStart, 0F,
+                xEnd, 0F,
+                xEnd, bitmap.height.toFloat(),
+                xStart, bitmap.height.toFloat()
             )
 
             val dst = floatArrayOf(
-                    foldItemWidth * i, if (isEven) 0F else depth,
-                    foldItemWidth * (i + 1), if (isEven) depth else 0F,
-                    foldItemWidth * (i + 1), if (isEven) bitmap.height.toFloat() + depth else bitmap.height.toFloat(),
-                    foldItemWidth * i, if (isEven) bitmap.height.toFloat() else bitmap.height.toFloat() + depth
+                foldItemWidth * i,
+                if (isEven) 0F else depth,
+                foldItemWidth * (i + 1),
+                if (isEven) depth else 0F,
+                foldItemWidth * (i + 1),
+                if (isEven) bitmap.height.toFloat() + depth else bitmap.height.toFloat(),
+                foldItemWidth * i,
+                if (isEven) bitmap.height.toFloat() else bitmap.height.toFloat() + depth
             )
 
             foldMatrices[i].setPolyToPoly(src, 0, dst, 0, src.size.shr(1))
