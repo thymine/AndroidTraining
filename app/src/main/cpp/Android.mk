89ebe133f6f14a2e7db57c1561bb5d44bcebb918
zhangxiangdong <418033+zhangxiangdong28@user.noreply.gitee.com>
2023-03-09 21:21:14 +0800

    test logcat

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -4,8 +4,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := hello-jni
 
+LOCAL_LDLIBS := -llog
+
 # We recommend avoiding absolute file paths; relative paths make your Android.mk file more portable.
 # Always use Unix-style forward slashes (/) in build files. The build system does not handle Windows-style backslashes (\) properly.
-LOCAL_SRC_FILES := $(LOCAL_PATH)/hello-jni.c
+LOCAL_SRC_FILES := $(LOCAL_PATH)/hello-jni.cpp $(LOCAL_PATH)/stl/container.cpp
 
 include $(BUILD_SHARED_LIBRARY)
