60d54a1ea0ab0746e8934ac1b47ed2d3ca5888e1
zhangxiangdong <418033+zhangxiangdong28@user.noreply.gitee.com>
2023-10-10 17:14:44 +0800

    Layouts

--- app/src/main/java/me/zhang/laboratory/ui/compose/ComposableActivity.kt
+++ app/src/main/java/me/zhang/laboratory/ui/compose/ComposableActivity.kt
@@ -3,50 +3,87 @@ package me.zhang.laboratory.ui.compose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import me.zhang.laboratory.R
 
 class ComposableActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Hello Android")
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
+val modifier = Modifier
+    .background(Color.DarkGray)
+    .border(1.dp, Color.Green)
+val color = Color.White
+
 @Composable
-fun MessageCard(name: String) {
-    Card(
-        border = BorderStroke(
-            Dp(1f),
-            Color.Blue
-        )
-    ) {
-        Text(
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.scenery),
+            contentDescription = "Contact profile picture",
             modifier = Modifier
-                .padding(Dp(12f))
-                .background(Color.DarkGray)
-                .border(Dp(1f), Color.Green),
-            text = "Hello $name!",
-            color = Color.White,
-            fontSize = 24.sp,
+                .size(48.dp)
+                .clip(RoundedCornerShape(CornerSize(8.dp))),
+            contentScale = ContentScale.Crop
         )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                modifier = modifier,
+                color = color,
+                fontSize = 18.sp,
+            )
+
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = msg.body,
+                modifier = modifier,
+                color = color,
+                fontSize = 14.sp,
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
