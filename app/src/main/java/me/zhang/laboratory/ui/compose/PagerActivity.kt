5521916064e28f1769c2d8f798cf215ee4d365ae
zhangxiangdong <418033+zhangxiangdong28@user.noreply.gitee.com>
2023-11-27 14:05:24 +0800

    Content padding

--- app/src/main/java/me/zhang/laboratory/ui/compose/PagerActivity.kt
+++ app/src/main/java/me/zhang/laboratory/ui/compose/PagerActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
@@ -32,6 +34,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import com.google.android.material.math.MathUtils.lerp
 import kotlinx.coroutines.launch
@@ -41,6 +44,16 @@ private const val TAG = "PagerActivity"
 
 @OptIn(ExperimentalFoundationApi::class)
 class PagerActivity : AppCompatActivity() {
+
+    private val twoPagesPerViewport = object : PageSize {
+        override fun Density.calculateMainAxisPageSize(
+            availableSpace: Int,
+            pageSpacing: Int
+        ): Int {
+            return (availableSpace - 2 * pageSpacing) / 2
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { Main() }
@@ -58,14 +71,14 @@ class PagerActivity : AppCompatActivity() {
             HorizontalPager(
                 beyondBoundsPageCount = 3,
                 state = horizontalPagerState,
-                modifier = Modifier
-                    .fillMaxWidth()
+                contentPadding = PaddingValues(16.dp),
+//                pageSize = PageSize.Fixed(256.dp)
+                pageSize = twoPagesPerViewport
             ) { page ->
                 Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
                     Card(
                         Modifier
                             .fillMaxWidth()
-                            .padding(16.dp)
                             .height(200.dp)
                             .graphicsLayer {
                                 // Calculate the absolute offset for the current page from the
@@ -83,17 +96,17 @@ class PagerActivity : AppCompatActivity() {
                                     1f - pageOffset.coerceIn(0f, 1f)
                                 )
 
-                                scaleX = lerp(
-                                    0.75f,
-                                    1f,
-                                    1f - pageOffset.coerceIn(0f, 1f)
-                                )
-
-                                scaleY = lerp(
-                                    0.75f,
-                                    1f,
-                                    1f - pageOffset.coerceIn(0f, 1f)
-                                )
+//                                scaleX = lerp(
+//                                    0.85f,
+//                                    1f,
+//                                    1f - pageOffset.coerceIn(0f, 1f)
+//                                )
+//
+//                                scaleY = lerp(
+//                                    0.85f,
+//                                    1f,
+//                                    1f - pageOffset.coerceIn(0f, 1f)
+//                                )
                             }
                     ) {
                         // Card content
